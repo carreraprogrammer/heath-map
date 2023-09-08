afa3bb10f9dcc6fc830a3e4f5ba292e283805c8a
Daniel <carreraprogrammer@gmail.com>
2023-09-08 18:09:18 -0500

    Add x axis

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ const link = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceDat
 let monthlyVariance;
 let baseTemp;
 
+let width = 1200;
+let height = 600;
+let padding =  60;
+
+let canvas = d3.select("#canvas")
+canvas.attr('width', width)
+canvas.attr('height', height)
+
+let generateScales = () => {
+  xScale = d3.scaleLinear()
+             .range([padding, width - padding])
+}
+
+let drawCells = () => {
+
+}
+
+let drawAxes = () => {
+  let xAxis = d3.axisBottom(xScale)
+
+  canvas.append('g')
+        .call(xAxis)
+        .attr('transform', 'translate(0,' + (height - padding) + ')')
+        
+}
 
 fetch(link)
   .then(function(response) {
@@ -11,7 +36,11 @@ fetch(link)
   .then(function(data) {
     monthlyVariance = data.monthlyVariance;
     baseTemp = data.baseTemperature
-    console.log(baseTemp);
+    console.log(baseTemp)
+    console.log(monthlyVariance)
+    generateScales();
+    drawCells();
+    drawAxes();
   })
   .catch(function(error) {
     console.error("Error:", error);
